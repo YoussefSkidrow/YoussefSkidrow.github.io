d7ddd4b04f5c40e58574e5a4289e182eae41aa2a
loqiemon <vagabund3219@gmail.com>
2024-06-19 20:12:39 +0300

    123

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const jsonServer = require('json-server')
+const cors = require('cors')
 
 const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
+// Настройка CORS
+server.use(cors({
+  origin: 'https://ck14375.tw1.ru', // Разрешить доступ только с http://example.com
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешить только эти HTTP-методы
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-HTTP-Method-Override'] // Разрешить только эти заголовки
+}))
 
 server.use(middlewares)
 server.use('/api', router)
